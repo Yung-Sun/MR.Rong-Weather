40483583a0d0dae0dfc4d453aa2c42b24fbb7c32
SamRong <YungSun3307@163.com>
2021-03-27 23:20:34 +0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// app.js
+let latitude, longitude
+App({
+    globalData: {
+        latitude: null,
+        longitude: null
+    }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,67 @@
+// index.js
+// 获取应用实例
+const app = getApp()
+let _page, _this;
+let getLocation = () => {
+  wx.getLocation({
+    type: 'wgs84',
+    success(res) {
+      const latitude = res.latitude
+      const longitude = res.longitude
+      console.log('纬度：' + latitude + '，经度：' + longitude)
+      _this.getDistrict(latitude, longitude)
+    }
+  })
+}
+
+Page({
+  data: {
+    district: '定位中',
+    districtId: undefined,
+    text: '-',
+    iconSrc: '../../images/icon/999.png'
+  },
+
+  // 页面初始化时获取位置信息
+  onLoad() {
+    _this = this;
+    getLocation()
+
+  },
+  getDistrict(latitude, longitude) {
+    _page = this;
+    wx.request({
+      url: `https://geoapi.qweather.com/v2/city/lookup?key=74be5e8b8bdb46ca970b3703ae3f165d&location=${longitude},${latitude}`,
+      success: function (res) {
+        const districtId = res.data.location[0].id
+        _page.setData({
+          district: res.data.location[0].name,
+          districtId: res.data.location[0].id
+        })
+        _page.getNowWeather(districtId)
+        _page.getFutureWeather(districtId)
+      }
+    })
+  },
+  getNowWeather(districtId) {
+    _page = this;
+    wx.request({
+      url: `https://devapi.qweather.com/v7/weather/now?key=74be5e8b8bdb46ca970b3703ae3f165d&location=${districtId}`,
+      success: function (res) {
+        let iconId = res.data.now.icon
+        let iconSrc = `../../images/icon/${iconId}.png`
+        _page.setData({ ...res.data.now, iconSrc })
+      }
+    })
+  },
+  getFutureWeather(districtId) {
+    wx.request({
+      url: `https://devapi.qweather.com/v7/weather/3d?key=74be5e8b8bdb46ca970b3703ae3f165d&location=${districtId}`,
+      success: function (res) {
+        let todayRowData, tomorrowRowData, afterTomorrowRowData
+        [todayRowData, tomorrowRowData, afterTomorrowRowData] = res.data.daily
+        _page.setData({ todayRowData, tomorrowRowData, afterTomorrowRowData })
+      }
+    })
+  }
+})
